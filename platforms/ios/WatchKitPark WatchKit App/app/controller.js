bf2f0d17f1f38300909e2dddac57ac4e8a2cf9fc
Antony Bello <antony.bello@pomona.edu>
2015-07-17 17:43:53 -0700

    Table controller works

--- platforms/ios/WatchKitPark WatchKit App/app/controller.js
+++ platforms/ios/WatchKitPark WatchKit App/app/controller.js
@@ -1,74 +1,103 @@
 console.log("Hello World!");
 
 var InterfaceController = WKInterfaceController.extend({
-    awakeWithContext: function(context) {
-        this.super.awakeWithContext(context);
-      this._optionsTable.setNumberOfRowsWithRowType(2,"tableRowController");
-       var row1 = this._optionsTable.rowControllerAtIndex(0);
-       var row2 = this._optionsTable.rowControllerAtIndex(1);
-       row1.interfaceLabel.setText("Park");
-       row1.interfaceImage.setImage(UIImage.imageNamed("transport122"));
-                                                       
-       console.log("loaded?");
-    },
-    willActivate: function() {
-        this.super.willActivate();
-        console.log("InterfaceController: willActivate");
-    },
-    didDeactivate: function() {
-        this.super.didDeactivate();
-        console.log("InterfaceController: didDeactivate");
-    },
-    loadTable: function() {
-        this._optionsTable.setNumberOfRowsWithRowType(2,"tableRowController");
-        var row1 = this._optionsTable.rowControllerAtIndex(0);
-        var row2 = this._optionsTable.rowControllerAtIndex(1);
-        console.log("loaded?");
-    },
-    optionsTable: function() {
-        return this._optionsTable;
-    },
-    "setOptionsTable:": function(value) {
-        this._optionsTable = value;
-        console.log("Set table: " + value);
-    }
+  awakeWithContext: function(context) {
+    this.super.awakeWithContext(context);
+    this._optionsTable.setNumberOfRowsWithRowType(2, "tableRowController");
+    var row1 = this._optionsTable.rowControllerAtIndex(0);
+    var row2 = this._optionsTable.rowControllerAtIndex(1);
+    row1._interfaceLabel.setText("Park");
+    //       row1._interfaceImage.setImage(UIImage.imageNamed("transport122.png"));
+    row2._interfaceLabel.setText("Time remaining");
+    //       row2._interfaceImage.setImage(UIImage.imageNamed("clock136.png"));
+    console.log("loaded?");
+  },
+  willActivate: function() {
+    this.super.willActivate();
+    console.log("InterfaceController: willActivate");
+  },
+  didDeactivate: function() {
+    this.super.didDeactivate();
+    console.log("InterfaceController: didDeactivate");
+  },
+  optionsTable: function() {
+    return this._optionsTable;
+  },
+  "setOptionsTable:": function(value) {
+    this._optionsTable = value;
+    console.log("Set table: " + value);
+  }
 }, {
-    name: "InterfaceController",
-    exposedMethods: {
-        loadTable: { returns: interop.types.void, params: [] },
-        optionsTable: { returns: interop.types.id, params: [] },
-        "setOptionsTable:": { returns: interop.types.void, params: [interop.types.id] }
+  name: "InterfaceController",
+  exposedMethods: {
+    loadTable: {
+      returns: interop.types.void,
+      params: []
+    },
+    optionsTable: {
+      returns: interop.types.id,
+      params: []
+    },
+    "setOptionsTable:": {
+      returns: interop.types.void,
+      params: [interop.types.id]
     }
+  }
+});
+
+var TableRowController = NSObject.extend({
+  //  interfaceImage: function() { return this._interfaceImage; },
+  interfaceLabel: function() {
+    return this._interfaceLabel;
+  },
+  //  "setInterfaceImage:": function(image) { this._interfaceImage = image; },
+  "setInterfaceLabel:": function(label) {
+    this._interfaceLabel = label;
+  }
+}, {
+  name: "TableRowController",
+  exposedMethods: {
+    //        interfaceImage: { returns: interop.types.id, params: [] },
+    interfaceLabel: {
+      returns: interop.types.id,
+      params: []
+    },
+    "setInterfaceLabel:": {
+      returns: interop.types.void,
+      params: [interop.types.id]
+    }
+    //        "setInterfaceImage:": { returns: interop.types.void, params: [interop.types.id] }
+  }
 });
 
 var NotificationController = WKUserNotificationInterfaceController.extend({
-    willActivate: function() {
-        this.super.willActivate();
-        console.log("NotificationController: willActivate");
-    },
-    didDeactivate: function() {
-        this.super.didDeactivate();
-        console.log("NotificationController: didDeactivate");
-    }
+  willActivate: function() {
+    this.super.willActivate();
+    console.log("NotificationController: willActivate");
+  },
+  didDeactivate: function() {
+    this.super.didDeactivate();
+    console.log("NotificationController: didDeactivate");
+  }
 }, {
-    name: "NotificationController"
+  name: "NotificationController"
 });
 
 var GlanceController = WKInterfaceController.extend({
-    awakeWithContext: function(context) {
-        this.super.awakeWithContext(context);
-        console.log("GlanceController: awakeWithContext");
-    },
-    willActivate: function() {
-        this.super.willActivate();
-        console.log("GlanceController: willActivate");
-    },
-    didDeactivate: function() {
-        this.super.didDeactivate();
-        console.log("GlanceController: didDeactivate");
-    }
+  awakeWithContext: function(context) {
+    this.super.awakeWithContext(context);
+    console.log("GlanceController: awakeWithContext");
+  },
+  willActivate: function() {
+    this.super.willActivate();
+    console.log("GlanceController: willActivate");
+  },
+  didDeactivate: function() {
+    this.super.didDeactivate();
+    console.log("GlanceController: didDeactivate");
+  }
 }, {
-    name: "GlanceController"
+  name: "GlanceController"
 });
 
 console.log("declared controllers");
